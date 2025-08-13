8949fb1741ab5c587f9f0a7c6700f7014c96d51f
Mikhail Kortikov <kortikov.m.r@gmail.com>
2025-08-14 00:23:19 +0300

    Add log flag

--- StateRoots/LogAppStateRootBehaviour.cs
+++ StateRoots/LogAppStateRootBehaviour.cs
@@ -1,22 +1,25 @@
 ﻿using System.Threading.Tasks;
 using AppStructure;
 using DingoProjectAppStructure.Core.AppRootCore;
-using DingoProjectAppStructure.Core.AppRootCore.UnityLifeTime;
 using UnityEngine;
 
 namespace DingoProjectAppStructure.StateRoots
 {
     public class LogAppStateRootBehaviour : AnimatableAppStateBehaviour
     {
+        [SerializeField] private bool _enableLog;
+        
         public override Task EnableOnTransferAsync(TransferInfo<string> transferInfo)
         {
-            Debug.Log($"Enable {transferInfo}", this);
+            if (_enableLog)
+                Debug.Log($"Enable {transferInfo}", this);
             return base.EnableOnTransferAsync(transferInfo);
         }
 
         protected override void DisableCompletely(TransferInfo<string> transferInfo)
         {
-            Debug.Log($"Disable {transferInfo}", this);
+            if (_enableLog)
+                Debug.Log($"Disable {transferInfo}", this);
             base.DisableCompletely(transferInfo);
         }
     }
