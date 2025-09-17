76b9bf2bfd676633a352781d50e51ae8cc19c38e
Mikhail Kortikov <kortikov.m.r@gmail.com>
2025-09-17 17:54:51 +0300

    SubGameController

---
+++ SceneRoot/AppStateControllerStaticStateElementWrapper.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using DingoProjectAppStructure.Core;
+using DingoProjectAppStructure.Core.AppRootCore;
+using DingoProjectAppStructure.Core.Model;
+using UnityEngine;
+
+namespace DingoProjectAppStructure.SceneRoot
+{
+    public class AppStateControllerStaticStateElementWrapper : AppStaticElementBehaviour
+    {
+        [SerializeField] private AppStateController _appStateController;
+        
+        public override void PreInitialize()
+        {
+            _appStateController.AppViewRoot.PreInitialize();
+            base.PreInitialize();
+        }
+
+        public override async Task InitializeAsync()
+        {
+            await _appStateController.AppViewRoot.InitializeAsync();
+            await base.InitializeAsync();
+        }
+
+        public override async Task BindAsync(AppModelRoot appModel)
+        {
+            await _appStateController.AppViewRoot.BindAsync(appModel);
+            await base.BindAsync(appModel);
+        }
+
+        public override async Task PostInitializeAsync()
+        {
+            await _appStateController.AppViewRoot.PostInitializeAsync();
+            await base.PostInitializeAsync();
+        }
+    }
+}
---
+++ SceneRoot/SubGameController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using DingoProjectAppStructure.Core;
+using DingoUnityExtensions.MonoBehaviours.Singletons;
+using UnityEngine;
+
+namespace DingoProjectAppStructure.SceneRoot
+{
+    public abstract class SubGameController<TThis> : ProtectedSingletonBehaviour<TThis> where TThis : SubGameController<TThis>
+    {
+        [SerializeField] private AppStateController _appStateController;
+        
+        public static IStateController State => Instance._appStateController;
+        public static List<string> States => Instance._appStateController.States;
+    }
+}
